6241be264ead761d00d8ddf0c45bc3da448fb7cd
vshadrin1991 <vital.shadrin2@gmail.com>
2026-09-26 21:57:55 +0300

    clean-hop v10.1

--- clean-hop-v10/sw.js
+++ clean-hop-v10/sw.js
@@ -1,7 +1,7 @@
 /* Энергопрыжок - offline copy of the game.
    Every file is served from the device first. When any file changes, bump
    VERSION so installed copies pick up the update on their next online visit. */
-var VERSION = 'clean-hop-v10-4';
+var VERSION = 'clean-hop-v10-5';
 var FILES = [
   './', 'index.html', 'manifest.webmanifest',
   'icon-180.png', 'icon-192.png', 'icon-512.png',
